84eee0f858c3c92cbe3962fdca40dfe3bcf7ba55
Ashwak Shaik <arrancarnelliel3@gmail.com>
2026-04-03 13:40:22 +0000

    updated the app with fix and enabled rollback deployment feature

---
+++ app/app.py
@@ -0,0 +1,42 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+healthy = True
+
+
+@app.route("/")
+def home():
+    return "Hello from Kubernetes(K8s)!"
+
+
+@app.route("/live")
+def live():
+    return "OK", 200
+
+
+@app.route("/ready")
+def ready():
+    if healthy:
+        return "OK", 200
+    return "FAIL", 500
+
+
+# Break the app (simulate failure)
+@app.route("/break")
+def break_app():
+    global healthy
+    healthy = False
+    return "App is now NOT READY"
+
+
+# Recover the app
+@app.route("/fix")
+def fix_app():
+    global healthy
+    healthy = True
+    return "App is READY again"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
